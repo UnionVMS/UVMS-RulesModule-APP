9e0fe558aebc3da8ae8f0d5ed8fa88d8c291d025
emlynnjoh <emlynnjoh@mycompany.com>
2015-10-08 15:51:16 +0000

    @Review Johanna, Myggan Update namespace
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Rules/APP@5348 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- model/src/main/java/eu/europa/ec/fisheries/uvms/rules/model/mapper/RulesModuleRequestMapper.java
+++ model/src/main/java/eu/europa/ec/fisheries/uvms/rules/model/mapper/RulesModuleRequestMapper.java
@@ -1,6 +1,6 @@
 package eu.europa.ec.fisheries.uvms.rules.model.mapper;
 
-import eu.europa.ec.fisheries.schema.movement.v1.MovementBaseType;
+import eu.europa.ec.fisheries.schema.rules.movement.v1.MovementBaseType;
 import eu.europa.ec.fisheries.schema.rules.module.v1.RulesModuleMethod;
 import eu.europa.ec.fisheries.schema.rules.module.v1.SetMovementReportRequest;
 import eu.europa.ec.fisheries.uvms.rules.model.exception.RulesModelMapperException;
