f34114ca584a985d579792a29b05c45531aa68ff
emlynnjoh <emlynnjoh@mycompany.com>
2015-10-26 08:17:19 +0000

    @Review Johanna, Myggan Update SetMovementReport with PluginType
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Rules/APP@5927 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- model/src/main/java/eu/europa/ec/fisheries/uvms/rules/model/mapper/RulesModuleRequestMapper.java
+++ model/src/main/java/eu/europa/ec/fisheries/uvms/rules/model/mapper/RulesModuleRequestMapper.java
@@ -1,5 +1,6 @@
 package eu.europa.ec.fisheries.uvms.rules.model.mapper;
 
+import eu.europa.ec.fisheries.schema.rules.exchange.v1.PluginType;
 import eu.europa.ec.fisheries.schema.rules.module.v1.RulesModuleMethod;
 import eu.europa.ec.fisheries.schema.rules.module.v1.SetMovementReportRequest;
 import eu.europa.ec.fisheries.schema.rules.movement.v1.RawMovementType;
@@ -7,9 +8,10 @@ import eu.europa.ec.fisheries.uvms.rules.model.exception.RulesModelMapperExcepti
 
 public class RulesModuleRequestMapper {
 
-    public static String createSetMovementReportRequest(RawMovementType rawMovementType) throws RulesModelMapperException {
+    public static String createSetMovementReportRequest(PluginType type, RawMovementType rawMovementType) throws RulesModelMapperException {
         SetMovementReportRequest request = new SetMovementReportRequest();
         request.setMethod(RulesModuleMethod.SET_MOVEMENT_REPORT);
+        request.setType(type);
         request.setRequest(rawMovementType);
         return JAXBMarshaller.marshallJaxBObjectToString(request);
     }
