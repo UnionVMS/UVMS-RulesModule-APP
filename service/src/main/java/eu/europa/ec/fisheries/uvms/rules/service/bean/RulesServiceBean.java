104ef033ff3a93daa1e49bf1809a4032d62bd96b
emthorean <emthorean@mycompany.com>
2015-11-04 10:36:08 +0000

    Reverted previous commit
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Rules/APP@6325 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/bean/RulesServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/bean/RulesServiceBean.java
@@ -1,12 +1,10 @@
 package eu.europa.ec.fisheries.uvms.rules.service.bean;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.enterprise.event.Event;
@@ -39,20 +37,6 @@ import eu.europa.ec.fisheries.uvms.vessel.model.exception.VesselModelMapperExcep
 import eu.europa.ec.fisheries.uvms.vessel.model.mapper.VesselModuleRequestMapper;
 import eu.europa.ec.fisheries.uvms.vessel.model.mapper.VesselModuleResponseMapper;
 import eu.europa.ec.fisheries.wsdl.vessel.types.*;
-import org.drools.template.parser.DefaultTemplateContainer;
-import org.drools.template.parser.TemplateContainer;
-import org.drools.template.parser.TemplateDataListener;
-import org.kie.api.KieBase;
-import org.kie.api.KieBaseConfiguration;
-import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.Message;
-import org.kie.api.builder.ReleaseId;
-import org.kie.api.runtime.KieContainer;
-import org.kie.api.runtime.KieSession;
-import org.kie.api.runtime.KieSessionConfiguration;
-import org.kie.internal.io.ResourceFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -112,8 +96,8 @@ public class RulesServiceBean implements RulesService {
     @TicketEvent
     private Event<NotificationMessage> ticketEvent;
 
-//    @Inject
-//    RulesValidator rulesValidator;
+    @Inject
+    RulesValidator rulesValidator;
 
     /**
      * {@inheritDoc}
@@ -566,8 +550,8 @@ public class RulesServiceBean implements RulesService {
 
             RawMovementFact rawFact = RulesUtil.mapRawMovementFact(rawMovementType, mobileTerminal, pluginType);
 
-//            rulesValidator.evaluate(rawFact);
-            evaluate(rawFact);
+            rulesValidator.evaluate(rawFact);
+//            evaluate(rawFact);
             auditTimestamp = auditLog("Time to validate sanity:", auditTimestamp);
 
             // MovementComChannelType.FLUX -> assetId
@@ -667,8 +651,8 @@ public class RulesServiceBean implements RulesService {
             // TODO:
             // Verify how spatial data can be validated
 
-//            rulesValidator.evaluate(movementFact);
-            evaluate(movementFact);
+            rulesValidator.evaluate(movementFact);
+//            evaluate(movementFact);
             auditTimestamp = auditLog("Time to validate rules:", auditTimestamp);
 
             persistThisMovementReport(vesselGuid, movement);
@@ -812,138 +796,6 @@ public class RulesServiceBean implements RulesService {
             LOG.error("[ Error when getting ticket by GUID ] {}", e.getMessage());
             throw new RulesServiceException("[ Error when getting ticket by GUID. ]");
         }
-    }
-
-
-
-
-    /////////////////// Emergency fix - moving all of RulesValidator in here!
-
-//    private final static Logger LOG = LoggerFactory.getLogger(RulesValidator.class);
-    private static final String SANITY_RESOURCE_DRL = "/rules/SanityRules.drl";
-    private static final String TIMER_DRL = "/rules/TimerRules.drl";
-    private static final String CUSTOM_RULE_TEMPLATE = "/templates/CustomRulesTemplate.drt";
-    private static final String CUSTOM_RULE_DRL = "src/main/resources/rules/TemplateRules.drl";
-
-    @Inject
-    RulesService rulesService;
-
-    private KieBase kbase;
-    private KieSessionConfiguration ksconf;
-
-    private KieServices kservices;
-    private KieContainer kcontainer;
-    private KieFileSystem kfs;
-
-    @PostConstruct
-    public void postConstruct() {
-        init();
-    }
-
-    private void init() {
-        LOG.info("Initializing Rules Validator");
-
-        kservices = KieServices.Factory.get();
-
-        kfs = kservices.newKieFileSystem();
-        kfs.write(ResourceFactory.newClassPathResource(SANITY_RESOURCE_DRL));
-//        kfs.write(ResourceFactory.newClassPathResource(TIMER_DRL));
-        KieBuilder kbuilder = kservices.newKieBuilder(kfs);
-        kbuilder.buildAll();
-
-        // Check for errors
-        if (kbuilder.getResults().hasMessages(Message.Level.ERROR)) {
-            throw new IllegalArgumentException(kbuilder.getResults().toString());
-        }
-
-        // Get the Release ID (mvn style: groupId, artifactId,version)
-        ReleaseId releaseId = kbuilder.getKieModule().getReleaseId();
-        // ReleaseId releaseId = kservices.newReleaseId("kits.drools.testrules",
-        // "testrules", "1.0-SNAPSHOT");
-        LOG.info("GroupId:{}, Artifact:{}, Version:{}", releaseId.getGroupId(), releaseId.getArtifactId(), releaseId.getVersion());
-
-        // Create the Container, wrapping the KieModule with the given ReleaseId
-        kcontainer = kservices.newKieContainer(releaseId);
-
-        // Configure and create the KieBase
-        KieBaseConfiguration kbconf = kservices.newKieBaseConfiguration();
-        kbase = kcontainer.newKieBase(kbconf);
-
-        // Configure and create the StatelessKieSession
-        ksconf = kservices.newKieSessionConfiguration();
-    }
-
-    public void evaluate(RawMovementFact fact) {
-        LOG.info("Verifying raw movement");
-
-        KieSession ksession = kbase.newKieSession(ksconf, null);
-
-        // Inject beans
-        ksession.setGlobal("rulesService", rulesService);
-        ksession.setGlobal("logger", LOG);
-
-        ksession.insert(fact);
-        ksession.fireAllRules();
-
-        ksession.dispose();
-    }
-
-    public void evaluate(MovementFact fact) {
-        LOG.info("Custom rule check");
-
-        // Fetch custom rules from DB
-        List<CustomRuleDto> rules = new ArrayList<CustomRuleDto>();
-        List<CustomRuleType> customRules = new ArrayList<CustomRuleType>();
-        try {
-            customRules = rulesService.getCustomRuleList();
-        } catch (RulesServiceException e) {
-            LOG.error("[ Error when getting rules ]");
-            // TODO: Throw exception???
-        }
-
-        if (customRules != null && !customRules.isEmpty()) {
-            // Add custom rules
-            rules = RulesUtil.parseRules(customRules);
-            String drl = generateDrl(CUSTOM_RULE_TEMPLATE, rules);
-            kfs.write(CUSTOM_RULE_DRL, drl);
-
-            // Create session
-            kservices.newKieBuilder(kfs).buildAll();
-            kcontainer = kservices.newKieContainer(kservices.getRepository().getDefaultReleaseId());
-            KieSession ksession = kcontainer.newKieSession();
-
-            // Inject beans
-            ksession.setGlobal("rulesService", rulesService);
-            ksession.setGlobal("logger", LOG);
-
-            ksession.insert(fact);
-            ksession.fireAllRules();
-
-            ksession.dispose();
-        }
-
-    }
-
-    private String generateDrl(String template, List<CustomRuleDto> rules) {
-        InputStream templateStream = this.getClass().getResourceAsStream(template);
-        TemplateContainer tc = new DefaultTemplateContainer(templateStream);
-        TemplateDataListener listener = new TemplateDataListener(tc);
-
-        int rowNum = 0;
-        for (CustomRuleDto rule : rules) {
-            listener.newRow(rowNum, 0);
-            listener.newCell(rowNum, 0, rule.getRuleName(), 0);
-            listener.newCell(rowNum, 1, rule.getExpression(), 0);
-            listener.newCell(rowNum, 2, rule.getAction(), 0);
-            listener.newCell(rowNum, 3, rule.getRuleGuid(), 0);
-            rowNum++;
-        }
-        listener.finishSheet();
-        String drl = listener.renderDRL();
-
-        LOG.info("Custom rule file:\n{}", drl);
-
-        return drl;
     }
 
 }
