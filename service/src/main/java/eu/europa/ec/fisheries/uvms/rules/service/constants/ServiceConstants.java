a78fe149eb424fb452899a5202beb4aa65b1496e
emthorean <emthorean@mycompany.com>
2015-11-05 14:26:08 +0000

    Missed in last commit
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/Modules/Rules/APP@6430 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/constants/ServiceConstants.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/constants/ServiceConstants.java
@@ -4,4 +4,9 @@ public class ServiceConstants {
 
     public static final String FIND_BY_NAME = "Parameter.findByName";
 
+    // Reserved words for rule actions
+    public static final String SEND_TO_CLOSEST_COUNTRY = "<SEND TO CLOSEST COUNTRY>";
+
+
+
 }
