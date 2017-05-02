fa23ce7a5fbd24b690cb0a718c432e2cb4041ef7
AdityaPadhy <aditya.padhy123@gmail.com>
2017-05-02 13:09:21 +0200

    Added IdType

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/fact/IdType.java
@@ -0,0 +1,39 @@
+/*
+ *
+ * Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries © European Union, 2015-2016.
+ *
+ * This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package eu.europa.ec.fisheries.uvms.rules.service.business.fact;
+
+/**
+ * Created by padhyad on 5/2/2017.
+ */
+public class IdType {
+
+    private String value;
+    private String schemeId;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getSchemeId() {
+        return schemeId;
+    }
+
+    public void setSchemeId(String schemeId) {
+        this.schemeId = schemeId;
+    }
+}
