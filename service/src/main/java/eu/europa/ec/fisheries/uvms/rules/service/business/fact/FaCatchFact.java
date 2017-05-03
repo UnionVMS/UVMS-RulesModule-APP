03bf68b03719c724cd18e0a0ad74a9e5d12c6054
emsanerac <Rachana.Sane@arhs-developments.com>
2017-05-03 15:59:04 +0200

    UNIONVMS-2638: FACatchFact class added

--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/fact/FaCatchFact.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/fact/FaCatchFact.java
@@ -15,20 +15,159 @@ package eu.europa.ec.fisheries.uvms.rules.service.business.fact;
 
 import eu.europa.ec.fisheries.schema.rules.template.v1.FactType;
 import eu.europa.ec.fisheries.uvms.rules.service.business.AbstractFact;
+import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._20.AAPProduct;
+
+import java.util.List;
 
 /**
  * Created by padhyad on 4/21/2017.
  */
 public class FaCatchFact extends AbstractFact {
 
-    private String typeCode;
+    private CodeType typeCode;
+    private CodeType speciesCode;
+    private List<AAPProduct> resultAAPProduct;
+    private MeasureType unitQuantity;
+    private String fishingActivityTypeCode;
+    private CodeType activityRelatedFLUXLocationId;
+    private MeasureType weightMeasure;
+    private CodeType weighingMeansCode;
+    private List<CodeType> sizeDistributionClassCode;
+    private CodeType sizeDistributionCategoryCode;
+    private List<CodeType> appliedAAPProcessTypeCodes;
+    private List<NumericType> appliedAAPProcessConversionFactor;
+    private List<CodeType> specifiedFLUXLocationId;
+    private List<CodeType> resultAAPProductPackagingTypeCode;
+    private List<MeasureType> resultAAPProductPackagingUnitAverageWeightMeasure;
+    private List<MeasureType> resultAAPProductUnitQuantity;
 
-    public String getTypeCode() {
+
+    public CodeType getTypeCode() {
         return typeCode;
     }
 
-    public void setTypeCode(String typeCode) {
+    public void setTypeCode(CodeType typeCode) {
         this.typeCode = typeCode;
+    }
+
+    public CodeType getSpeciesCode() {
+        return speciesCode;
+    }
+
+    public void setSpeciesCode(CodeType speciesCode) {
+        this.speciesCode = speciesCode;
+    }
+
+    public List<AAPProduct> getResultAAPProduct() {
+        return resultAAPProduct;
+    }
+
+    public void setResultAAPProduct(List<AAPProduct> resultAAPProduct) {
+        this.resultAAPProduct = resultAAPProduct;
+    }
+
+    public MeasureType getUnitQuantity() {
+        return unitQuantity;
+    }
+
+    public void setUnitQuantity(MeasureType unitQuantity) {
+        this.unitQuantity = unitQuantity;
+    }
+
+    public String getFishingActivityTypeCode() {
+        return fishingActivityTypeCode;
+    }
+
+    public void setFishingActivityTypeCode(String fishingActivityTypeCode) {
+        this.fishingActivityTypeCode = fishingActivityTypeCode;
+    }
+
+    public CodeType getActivityRelatedFLUXLocationId() {
+        return activityRelatedFLUXLocationId;
+    }
+
+    public void setActivityRelatedFLUXLocationId(CodeType activityRelatedFLUXLocationId) {
+        this.activityRelatedFLUXLocationId = activityRelatedFLUXLocationId;
+    }
+
+    public MeasureType getWeightMeasure() {
+        return weightMeasure;
+    }
+
+    public void setWeightMeasure(MeasureType weightMeasure) {
+        this.weightMeasure = weightMeasure;
+    }
+
+    public CodeType getWeighingMeansCode() {
+        return weighingMeansCode;
+    }
+
+    public void setWeighingMeansCode(CodeType weighingMeansCode) {
+        this.weighingMeansCode = weighingMeansCode;
+    }
+
+    public List<CodeType> getSizeDistributionClassCode() {
+        return sizeDistributionClassCode;
+    }
+
+    public void setSizeDistributionClassCode(List<CodeType> sizeDistributionClassCode) {
+        this.sizeDistributionClassCode = sizeDistributionClassCode;
+    }
+
+    public CodeType getSizeDistributionCategoryCode() {
+        return sizeDistributionCategoryCode;
+    }
+
+    public void setSizeDistributionCategoryCode(CodeType sizeDistributionCategoryCode) {
+        this.sizeDistributionCategoryCode = sizeDistributionCategoryCode;
+    }
+
+    public List<CodeType> getAppliedAAPProcessTypeCodes() {
+        return appliedAAPProcessTypeCodes;
+    }
+
+    public void setAppliedAAPProcessTypeCodes(List<CodeType> appliedAAPProcessTypeCodes) {
+        this.appliedAAPProcessTypeCodes = appliedAAPProcessTypeCodes;
+    }
+
+    public List<NumericType> getAppliedAAPProcessConversionFactor() {
+        return appliedAAPProcessConversionFactor;
+    }
+
+    public void setAppliedAAPProcessConversionFactor(List<NumericType> appliedAAPProcessConversionFactor) {
+        this.appliedAAPProcessConversionFactor = appliedAAPProcessConversionFactor;
+    }
+
+    public List<CodeType> getSpecifiedFLUXLocationId() {
+        return specifiedFLUXLocationId;
+    }
+
+    public void setSpecifiedFLUXLocationId(List<CodeType> specifiedFLUXLocationId) {
+        this.specifiedFLUXLocationId = specifiedFLUXLocationId;
+    }
+
+    public List<CodeType> getResultAAPProductPackagingTypeCode() {
+        return resultAAPProductPackagingTypeCode;
+    }
+
+    public void setResultAAPProductPackagingTypeCode(List<CodeType> resultAAPProductPackagingTypeCode) {
+        this.resultAAPProductPackagingTypeCode = resultAAPProductPackagingTypeCode;
+    }
+
+    public List<MeasureType> getResultAAPProductPackagingUnitAverageWeightMeasure() {
+        return resultAAPProductPackagingUnitAverageWeightMeasure;
+    }
+
+    public void setResultAAPProductPackagingUnitAverageWeightMeasure(List<MeasureType> resultAAPProductPackagingUnitAverageWeightMeasure) {
+        this.resultAAPProductPackagingUnitAverageWeightMeasure = resultAAPProductPackagingUnitAverageWeightMeasure;
+    }
+
+    public List<MeasureType> getResultAAPProductUnitQuantity() {
+        return resultAAPProductUnitQuantity;
+    }
+
+    public void setResultAAPProductUnitQuantity(List<MeasureType> resultAAPProductUnitQuantity) {
+        this.resultAAPProductUnitQuantity = resultAAPProductUnitQuantity;
     }
 
     public FaCatchFact() {
