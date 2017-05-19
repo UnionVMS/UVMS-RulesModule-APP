7e0d67d5f14a39f4c8d7459c093168d8da6724a1
stihft <stijn.hooft@lv.vlaanderen.be>
2017-05-19 17:04:09 +0200

    add empty fact classes Sprint Backlog Item #138581 - Rules en facts toevoegen aan engine

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/fact/SalesContactPersonFact.java
@@ -0,0 +1,18 @@
+package eu.europa.ec.fisheries.uvms.rules.service.business.fact;
+
+import eu.europa.ec.fisheries.schema.rules.template.v1.FactType;
+import eu.europa.ec.fisheries.uvms.rules.service.business.AbstractFact;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+public class SalesContactPersonFact extends AbstractFact {
+
+    @Override
+    public void setFactType() {
+        this.factType = FactType.SALES_CONTACT_PERSON;
+    }
+}
