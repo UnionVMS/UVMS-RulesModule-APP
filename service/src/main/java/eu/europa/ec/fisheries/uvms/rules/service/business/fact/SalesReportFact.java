818d63834bd3769280704eed8e14ccfc2f2fb4e3
stihft <stijn.hooft@lv.vlaanderen.be>
2017-08-10 14:06:03 +0200

    adapt the rules that should only trigger on a sales note / take over document Sprint Backlog Item #139181 - Implement rules level 1

--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/fact/SalesReportFact.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/fact/SalesReportFact.java
@@ -1,17 +1,17 @@
 package eu.europa.ec.fisheries.uvms.rules.service.business.fact;
 
 import eu.europa.ec.fisheries.schema.rules.template.v1.FactType;
-import eu.europa.ec.fisheries.schema.sales.AAPProductType;
-import eu.europa.ec.fisheries.schema.sales.AmountType;
-import eu.europa.ec.fisheries.schema.sales.SalesBatchType;
-import eu.europa.ec.fisheries.schema.sales.ValidationResultDocumentType;
+import eu.europa.ec.fisheries.schema.sales.*;
 import eu.europa.ec.fisheries.uvms.rules.service.business.SalesAbstractFact;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class SalesReportFact extends SalesAbstractFact {
+
+    public static final String MDR_FISH_FRESHNESS = "FISH_FRESHNESS";
 
     private IdType id;
     private CodeType itemTypeCode;
@@ -72,7 +72,7 @@ public class SalesReportFact extends SalesAbstractFact {
     }
 
     public boolean isSellerRoleNotSpecifiedForSalesNote(){
-        if(itemTypeCode != null && Objects.equals(itemTypeCode.getValue(), "SN") && !isEmpty(includedSalesDocuments))
+        if(isItemTypeEqualTo("SN") && !isEmpty(includedSalesDocuments))
         {
             for (SalesDocumentFact salesDocument:includedSalesDocuments) {
                 if (salesDocument == null || salesDocument.getSpecifiedSalesParties() == null){
@@ -97,10 +97,10 @@ public class SalesReportFact extends SalesAbstractFact {
     }
 
     public boolean isSellerRoleOrBuyerNotSpecifiedForSalesNoteWithPurchase(){
-        if(itemTypeCode != null && "SN".equals(itemTypeCode.getValue()) && !isEmpty(includedSalesDocuments)) {
+        if(isItemTypeEqualTo("SN") && !isEmpty(includedSalesDocuments)) {
             for (SalesDocumentFact salesDocument : includedSalesDocuments) {
                 // If the document does not have a price greater than zero it can not pass the test since it is not considered a purchase
-                if (salesDocument == null || isTotalZero(salesDocument)) {
+                if (isTotalZero()) {
                     return false;
                 }
 
@@ -126,7 +126,7 @@ public class SalesReportFact extends SalesAbstractFact {
     }
 
     public boolean isRecipientRoleNotSpecifiedForTakeOverDocument(){
-        if(itemTypeCode != null && Objects.equals(itemTypeCode.getValue(), "TOD") && !isEmpty(includedSalesDocuments))
+        if(isItemTypeEqualTo("TOD") && !isEmpty(includedSalesDocuments))
         {
             for (SalesDocumentFact salesDocument:includedSalesDocuments) {
                 if (salesDocument == null || salesDocument.getSpecifiedSalesParties() == null){
@@ -150,7 +150,7 @@ public class SalesReportFact extends SalesAbstractFact {
     }
 
     public boolean isFluxOrganizationNotSpecifiedOnAllSalesPartiesForTakeOverDocument(){
-        if(itemTypeCode != null && Objects.equals(itemTypeCode.getValue(), "TOD") && !isEmpty(includedSalesDocuments))
+        if(isItemTypeEqualTo("TOD") && !isEmpty(includedSalesDocuments))
         {
             for (SalesDocumentFact salesDocument:includedSalesDocuments) {
                 if (salesDocument == null || salesDocument.getSpecifiedSalesParties() == null){
@@ -168,9 +168,8 @@ public class SalesReportFact extends SalesAbstractFact {
         return false;
     }
 
-
     public boolean isSalesNoteIdentifierNotSpecifiedForTakeOverDocumentWithStoredProducts(){
-        if(itemTypeCode != null && Objects.equals(itemTypeCode.getValue(), "TOD") && !isEmpty(includedSalesDocuments))
+        if(isItemTypeEqualTo("TOD") && !isEmpty(includedSalesDocuments))
         {
             for (SalesDocumentFact salesDocument:includedSalesDocuments) {
                 if (isAnyProductSetWithStorageAsUsage(salesDocument)){
@@ -184,45 +183,199 @@ public class SalesReportFact extends SalesAbstractFact {
         return false;
     }
 
-    private boolean isAnyProductSetWithStorageAsUsage(SalesDocumentFact salesDocument){
-        if (salesDocument == null || isEmpty(salesDocument.getSpecifiedSalesBatches())){
+    public boolean isSalesNoteAndIsFreshnessNotPresent() {
+        return isItemTypeEqualTo("SN") && isFreshnessNotPresent();
+    }
+
+    public boolean isTakeOverDocumentAndDoesAFLUXOrganizationNotHaveAName() {
+        return isItemTypeEqualTo("TOD") && doesAFLUXOrganizationNotHaveAName();
+    }
+
+    public boolean isSalesNoteAndNotAllChargeAmountsAreGreaterThanOrEqualToZero() {
+        return isItemTypeEqualTo("SN") && notAllChargeAmountsAreGreaterThanOrEqualToZero();
+    }
+
+    public boolean isSalesNoteAndAtLeastOneChargeAmountIsNull() {
+        return isItemTypeEqualTo("SN") && isAChargeAmountNull();
+    }
+
+    public boolean isSalesNoteAndAnyChargeAmountIsEqualToZero() {
+        return isItemTypeEqualTo("SN") && isAChargeAmountZero();
+    }
+
+    private boolean isAChargeAmountNull() {
+        List<AmountType> chargeAmounts = getAllChargeAmounts();
+        for (AmountType amount : chargeAmounts) {
+            if (amount == null || amount.getValue() == null) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isAChargeAmountZero() {
+        List<AmountType> chargeAmounts = getAllChargeAmounts();
+        for (AmountType amount : chargeAmounts) {
+            if (amount != null && amount.getValue() != null && amount.getValue().compareTo(BigDecimal.ZERO) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean notAllChargeAmountsAreGreaterThanOrEqualToZero() {
+        List<AmountType> chargeAmounts = getAllChargeAmounts();
+        for (AmountType amount : chargeAmounts) {
+            if (amount != null && amount.getValue() != null && amount.getValue().compareTo(BigDecimal.ZERO) < 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<AmountType> getAllChargeAmounts() {
+        List<AAPProductType> products = getProducts();
+        SalesPriceType totalSalesPrice = getTotalSalesPrice();
+
+        List<AmountType> chargeAmounts = new ArrayList<>();
+        for (AAPProductType product: products) {
+            if (product != null && product.getTotalSalesPrice() != null) {
+                if (isEmpty(product.getTotalSalesPrice().getChargeAmounts())) {
+                    chargeAmounts.add(null);
+                } else{
+                    chargeAmounts.add(product.getTotalSalesPrice().getChargeAmounts().get(0));
+                }
+            }
+        }
+        if (totalSalesPrice != null) {
+            if (isEmpty(totalSalesPrice.getChargeAmounts())) {
+                chargeAmounts.add(null);
+            } else {
+                chargeAmounts.add(totalSalesPrice.getChargeAmounts().get(0));
+            }
+        }
+        return chargeAmounts;
+    }
+
+    private SalesPriceType getTotalSalesPrice() {
+        if (isEmpty(includedSalesDocuments) || includedSalesDocuments.get(0) == null) {
+            return null;
+        } else{
+            return includedSalesDocuments.get(0).getTotalSalesPrice();
+        }
+    }
+
+    private boolean doesAFLUXOrganizationNotHaveAName() {
+        if (!isEmpty(includedSalesDocuments) && includedSalesDocuments.get(0) != null) {
+            return doesAFLUXOrganizationNotHaveANameInSalesParties(includedSalesDocuments.get(0).getSpecifiedSalesParties()) ||
+                    doesAFLUXOrganizationNotHaveANameInVehicleTransportMeans(includedSalesDocuments.get(0).getSpecifiedVehicleTransportMeans());
+        } else {
             return false;
         }
+    }
 
-        for (SalesBatchType salesBatch : salesDocument.getSpecifiedSalesBatches()) {
-            if (salesBatch != null && !isEmpty(salesBatch.getSpecifiedAAPProducts())) {
-                for (AAPProductType aapProduct : salesBatch.getSpecifiedAAPProducts()) {
-                    if (aapProduct != null && aapProduct.getUsageCode() != null && aapProduct.getUsageCode().getValue() == "STO") {
+    private boolean doesAFLUXOrganizationNotHaveANameInVehicleTransportMeans(VehicleTransportMeansType specifiedVehicleTransportMeans) {
+        return specifiedVehicleTransportMeans != null &&
+                doesAFLUXOrganizationNotHaveAName(specifiedVehicleTransportMeans.getOwnerSalesParty());
+        }
+
+    private boolean doesAFLUXOrganizationNotHaveAName(SalesPartyType ownerSalesParty) {
+        if (ownerSalesParty != null) {
+            FLUXOrganizationType specifiedFLUXOrganization = ownerSalesParty.getSpecifiedFLUXOrganization();
+            return doesTheFLUXOrganizationNotHaveAName(specifiedFLUXOrganization);
+        }
+        return false;
+    }
+
+    private boolean doesTheFLUXOrganizationNotHaveAName(FLUXOrganizationType specifiedFLUXOrganization) {
+        return specifiedFLUXOrganization != null &&
+                (specifiedFLUXOrganization.getName() == null || isEmpty(specifiedFLUXOrganization.getName().getValue()));
+    }
+
+    private boolean doesAFLUXOrganizationNotHaveANameInSalesParties(List<SalesPartyFact> specifiedSalesParties) {
+        if (!isEmpty(specifiedSalesParties)) {
+            for (SalesPartyFact salesPartyFact : specifiedSalesParties) {
+                if (salesPartyFact != null) {
+                    if (doesTheFLUXOrganizationNotHaveAName(salesPartyFact.getSpecifiedFLUXOrganization())) {
                         return true;
                     }
                 }
+            }
+        }
+        return false;
+    }
+
+    private boolean isFreshnessNotPresent() {
+        return isFreshnessNotPresent(getProducts());
+    }
+
+    private boolean isFreshnessNotPresent(List<AAPProductType> specifiedAAPProducts) {
+        if (!isEmpty(specifiedAAPProducts)) {
+            for (AAPProductType aapProductType : specifiedAAPProducts) {
+                List<AAPProcessType> appliedAAPProcesses = aapProductType.getAppliedAAPProcesses();
+                if (!isEmpty(appliedAAPProcesses)) {
+                    for (AAPProcessType aapProcessType : aapProductType.getAppliedAAPProcesses()) {
+                        if (isFreshnessNotPresent(aapProcessType)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isFreshnessNotPresent(AAPProcessType aapProcessType) {
+        boolean foundFreshnessForThisAAPProcessType = false;
+        for (eu.europa.ec.fisheries.schema.sales.CodeType codeType : aapProcessType.getTypeCodes()) {
+            if (MDR_FISH_FRESHNESS.equals(codeType.getListID())) {
+                foundFreshnessForThisAAPProcessType = true;
+            }
+        }
+        return !foundFreshnessForThisAAPProcessType;
+    }
+
+    private boolean isItemTypeEqualTo(String type) {
+        return itemTypeCode != null && Objects.equals(itemTypeCode.getValue(), type);
+    }
+
+    private boolean isAnyProductSetWithStorageAsUsage(SalesDocumentFact salesDocument){
+        for (AAPProductType product : getProducts()) {
+            if (product != null && product.getUsageCode() != null && product.getUsageCode().getValue() == "STO") {
+                return true;
             }
         }
 
         return false;
     }
 
-    private boolean isTotalZero(SalesDocumentFact salesDocument){
-
-        if(salesDocument.getSpecifiedSalesBatches() == null || salesDocument.getSpecifiedSalesBatches().size() == 0)        {
-            return true;
-        }
-
-        for (SalesBatchType salesBatch :salesDocument.getSpecifiedSalesBatches()) {
-            if(!isEmpty(salesBatch.getSpecifiedAAPProducts())){
-                for (AAPProductType product: salesBatch.getSpecifiedAAPProducts()) {
-                    if (product != null && product.getTotalSalesPrice() != null
-                            && !isEmpty(product.getTotalSalesPrice().getChargeAmounts())){
-                        for (AmountType amount :product.getTotalSalesPrice().getChargeAmounts()) {
-                            if (amount != null && BigDecimal.ZERO.compareTo(amount.getValue()) == -1){
-                                return false;
-                            }
-                        }
+    private boolean isTotalZero(){
+        for (AAPProductType product: getProducts()) {
+            if (product != null && product.getTotalSalesPrice() != null
+                    && !isEmpty(product.getTotalSalesPrice().getChargeAmounts())){
+                for (AmountType amount :product.getTotalSalesPrice().getChargeAmounts()) {
+                    if (amount != null && BigDecimal.ZERO.compareTo(amount.getValue()) == -1){
+                        return false;
                     }
                 }
             }
         }
-
         return true;
+    }
+
+    private List<AAPProductType> getProducts() {
+        if (isEmpty(includedSalesDocuments) ||
+                includedSalesDocuments.get(0) == null ||
+                isEmpty(includedSalesDocuments.get(0).getSpecifiedSalesBatches())){
+            return new ArrayList<>();
+        }
+
+        List<AAPProductType> products = new ArrayList<>();
+        for (SalesBatchType salesBatch : includedSalesDocuments.get(0).getSpecifiedSalesBatches()) {
+            if (!isEmpty(salesBatch.getSpecifiedAAPProducts())) {
+                products.addAll(salesBatch.getSpecifiedAAPProducts());
+            }
+        }
+        return products;
     }
 }
