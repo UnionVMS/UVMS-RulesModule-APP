7315b728b688909a9e5ce2b6e01aecb395b05376
thomasfanto <thomas.fanto@consid.se>
2017-11-02 10:04:55 +0100

    removed two old instancevariable to avoid future confusion

--- message/src/main/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesResponseConsumerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesResponseConsumerBean.java
@@ -40,8 +40,6 @@ public class RulesResponseConsumerBean implements RulesResponseConsumer, ConfigM
 
     private ConnectionFactory connectionFactory;
 
-    private Connection connection = null;
-    private Session session = null;
 
     @PostConstruct
     private void init() {
