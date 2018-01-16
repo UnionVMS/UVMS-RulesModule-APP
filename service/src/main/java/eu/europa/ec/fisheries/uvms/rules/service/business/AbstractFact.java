4c6ce5dcd31818d51ed586e7cd5c703d8d602894
emrinalgr <rinaldi.gregory@gmail.com>
2018-01-16 11:25:35 +0100

    FA-L02-00-0416 - Fa notification of arrival template error

--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/AbstractFact.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/AbstractFact.java
@@ -491,7 +491,11 @@ public abstract class AbstractFact {
         if (valuesToMatch == null || valuesToMatch.length == 0 || CollectionUtils.isEmpty(codeTypes)) {
             return true;
         }
-        for (String val : valuesToMatch) {
+        codeTypes.removeAll(Collections.singleton(null));
+        List<String> valueList = Arrays.asList(valuesToMatch);
+        valueList.removeAll(Collections.singleton(null));
+
+        for (String val : valueList) {
             for (CodeType IdType : codeTypes) {
                 if (!val.equals(IdType.getListId())) {
                     return true;
