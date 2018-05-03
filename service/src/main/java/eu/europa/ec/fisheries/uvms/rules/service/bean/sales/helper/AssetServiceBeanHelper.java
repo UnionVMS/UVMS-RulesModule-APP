f72e00f1ab616aff99a83e59e9076f5980cb9c1c
MathiasBulte <bulte.mathias@gmail.com>
2018-05-03 14:07:34 +0200

    Task #146448: SALE-L03-00-0130 - Revert code that communicates with asset because asset isn't ready yet

--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/bean/sales/helper/AssetServiceBeanHelper.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/bean/sales/helper/AssetServiceBeanHelper.java
@@ -8,8 +8,6 @@ import eu.europa.ec.fisheries.uvms.commons.message.api.MessageException;
 import eu.europa.ec.fisheries.uvms.rules.message.constants.DataSourceQueue;
 import eu.europa.ec.fisheries.uvms.rules.message.consumer.RulesResponseConsumer;
 import eu.europa.ec.fisheries.uvms.rules.message.producer.RulesMessageProducer;
-import eu.europa.ec.fisheries.wsdl.asset.module.FindAssetHistoriesByCfrModuleResponse;
-import eu.europa.ec.fisheries.wsdl.asset.types.Asset;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -18,32 +16,32 @@ import java.util.List;
 
 @Singleton
 public class AssetServiceBeanHelper {
-
-    @EJB
-    private RulesMessageProducer messageProducer;
-
-    @EJB
-    private RulesResponseConsumer messageConsumer;
-
-    protected List<Asset> receiveMessageFromAsset(String correlationId) throws MessageException, AssetModelMarshallException {
-        TextMessage receivedMessage = messageConsumer.getMessage(correlationId, TextMessage.class);
-        return unmarshal(receivedMessage);
-    }
-
-    protected String sendMessageToAsset(String request) throws MessageException {
-        return messageProducer.sendDataSourceMessage(request, DataSourceQueue.ASSET);
-    }
-
-    protected List<Asset> unmarshal(TextMessage message) throws AssetModelMarshallException {
-        FindAssetHistoriesByCfrModuleResponse findAssetHistoriesByCfrModuleResponse = JAXBMarshaller.unmarshallTextMessage(message, FindAssetHistoriesByCfrModuleResponse.class);
-
-        return findAssetHistoriesByCfrModuleResponse.getAssetHistories();
-    }
-
-    public List<Asset> findHistoryOfAssetByCfr(String cfr) throws MessageException, AssetModelMarshallException {
-        String checkForUniqueIdRequest = AssetModuleRequestMapper.createFindAssetByCfrRequest(cfr);
-        String correlationID = sendMessageToAsset(checkForUniqueIdRequest);
-
-        return receiveMessageFromAsset(correlationID);
-    }
+//
+//    @EJB
+//    private RulesMessageProducer messageProducer;
+//
+//    @EJB
+//    private RulesResponseConsumer messageConsumer;
+//
+//    protected List<Asset> receiveMessageFromAsset(String correlationId) throws MessageException, AssetModelMarshallException {
+//        TextMessage receivedMessage = messageConsumer.getMessage(correlationId, TextMessage.class);
+//        return unmarshal(receivedMessage);
+//    }
+//
+//    protected String sendMessageToAsset(String request) throws MessageException {
+//        return messageProducer.sendDataSourceMessage(request, DataSourceQueue.ASSET);
+//    }
+//
+//    protected List<Asset> unmarshal(TextMessage message) throws AssetModelMarshallException {
+//        FindAssetHistoriesByCfrModuleResponse findAssetHistoriesByCfrModuleResponse = JAXBMarshaller.unmarshallTextMessage(message, FindAssetHistoriesByCfrModuleResponse.class);
+//
+//        return findAssetHistoriesByCfrModuleResponse.getAssetHistories();
+//    }
+//
+//    public List<Asset> findHistoryOfAssetByCfr(String cfr) throws MessageException, AssetModelMarshallException {
+//        String checkForUniqueIdRequest = AssetModuleRequestMapper.createFindAssetByCfrRequest(cfr);
+//        String correlationID = sendMessageToAsset(checkForUniqueIdRequest);
+//
+//        return receiveMessageFromAsset(correlationID);
+//    }
 }
