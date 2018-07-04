71192b7a7923b3bec70664b4a028e0be6b5463fa
emrinalgr <rinaldi.gregory@gmail.com>
2018-07-04 20:44:01 +0200

    fix query

--- domain/src/main/java/eu/europa/ec/fisheries/uvms/rules/dao/FaIdsPerTripDao.java
+++ domain/src/main/java/eu/europa/ec/fisheries/uvms/rules/dao/FaIdsPerTripDao.java
@@ -1,11 +1,13 @@
 package eu.europa.ec.fisheries.uvms.rules.dao;
 
-import eu.europa.ec.fisheries.uvms.commons.service.dao.AbstractDAO;
-import eu.europa.ec.fisheries.uvms.rules.entity.FaIdsPerTrip;
-
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import java.util.ArrayList;
 import java.util.List;
+
+import eu.europa.ec.fisheries.uvms.commons.service.dao.AbstractDAO;
+import eu.europa.ec.fisheries.uvms.rules.entity.FaIdsPerTrip;
+import org.apache.commons.collections.CollectionUtils;
 
 public class FaIdsPerTripDao extends AbstractDAO<FaIdsPerTrip> {
 
@@ -21,9 +23,13 @@ public class FaIdsPerTripDao extends AbstractDAO<FaIdsPerTrip> {
     }
 
     public List<FaIdsPerTrip> loadFADocumentIDByIdsByIds(List<String> inList) {
-        Query query = getEntityManager().createNamedQuery(FaIdsPerTrip.BY_TRIPIDSCHEMEIDFATYPEREPORTTYPE);
-        query.setParameter("idsList", inList);
-        return query.getResultList();
+        List<FaIdsPerTrip> list = new ArrayList<>();
+        if(CollectionUtils.isNotEmpty(inList)){
+            Query query = getEntityManager().createNamedQuery(FaIdsPerTrip.BY_TRIPIDSCHEMEIDFATYPEREPORTTYPE);
+            query.setParameter("idsList", inList);
+            list = query.getResultList();
+        }
+        return list;
     }
 
 }
