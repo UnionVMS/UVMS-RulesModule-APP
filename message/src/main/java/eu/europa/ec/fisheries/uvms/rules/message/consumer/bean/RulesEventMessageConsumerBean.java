29ab75bfaad0a1922fde8e7e55d606ccf3a6cfd1
emrinalgr <rinaldi.gregory@gmail.com>
2018-07-05 11:33:26 +0200

    add MDB config for activities

--- message/src/main/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesEventMessageConsumerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesEventMessageConsumerBean.java
@@ -63,7 +63,7 @@ import org.slf4j.MDC;
         @ActivationConfigProperty(propertyName = MessageConstants.DESTINATION_STR, propertyValue = MessageConstants.RULES_MESSAGE_IN_QUEUE_NAME),
         @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = "messageSelector IS NULL"),
         @ActivationConfigProperty(propertyName = "maxMessagesPerSessions", propertyValue = "1"),
-        @ActivationConfigProperty(propertyName = "initialRedeliveryDelay", propertyValue = "1000"),
+        @ActivationConfigProperty(propertyName = "initialRedeliveryDelay", propertyValue = "60000"),
         @ActivationConfigProperty(propertyName = "maximumRedeliveries", propertyValue = "3"),
         @ActivationConfigProperty(propertyName = "maxSessions", propertyValue = "1"),
 })
