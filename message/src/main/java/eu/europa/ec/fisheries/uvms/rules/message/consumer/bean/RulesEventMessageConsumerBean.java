97df43d2dbb015d9d3f81c231cb6cf1dcd56bf0d
emrinalgr <rinaldi.gregory@gmail.com>
2018-07-05 16:56:56 +0200

    fix retry

--- message/src/main/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesEventMessageConsumerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesEventMessageConsumerBean.java
@@ -63,8 +63,8 @@ import org.slf4j.MDC;
         @ActivationConfigProperty(propertyName = MessageConstants.DESTINATION_STR, propertyValue = MessageConstants.RULES_MESSAGE_IN_QUEUE_NAME),
         @ActivationConfigProperty(propertyName = "messageSelector", propertyValue = "messageSelector IS NULL"),
         @ActivationConfigProperty(propertyName = "maxMessagesPerSessions", propertyValue = "1"),
-        @ActivationConfigProperty(propertyName = "initialRedeliveryDelay", propertyValue = "300000"),
-        @ActivationConfigProperty(propertyName = "maximumRedeliveries", propertyValue = "1"),
+        @ActivationConfigProperty(propertyName = "initialRedeliveryDelay", propertyValue = "60000"),
+        @ActivationConfigProperty(propertyName = "maximumRedeliveries", propertyValue = "3"),
         @ActivationConfigProperty(propertyName = "maxSessions", propertyValue = "1"),
 })
 public class RulesEventMessageConsumerBean implements MessageListener {
