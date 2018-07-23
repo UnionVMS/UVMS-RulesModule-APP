b3a3ade8f5456dcc75ba63976db005c4f25f45c1
stihft <stijn.hooft@lv.vlaanderen.be>
2018-07-23 14:59:31 +0200

    #146448 re-enable SALE-L03-00-0130

--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/bean/sales/AssetServiceBean.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/bean/sales/AssetServiceBean.java
@@ -1,12 +1,19 @@
 package eu.europa.ec.fisheries.uvms.rules.service.bean.sales;
 
+import com.google.common.base.Optional;
+import eu.europa.ec.fisheries.uvms.asset.model.exception.AssetModelMarshallException;
+import eu.europa.ec.fisheries.uvms.commons.message.api.MessageException;
 import eu.europa.ec.fisheries.uvms.rules.service.AssetService;
 import eu.europa.ec.fisheries.uvms.rules.service.bean.sales.helper.AssetServiceBeanHelper;
+import eu.europa.ec.fisheries.wsdl.asset.types.Asset;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.time.DateTime;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 
 @Singleton
 @Slf4j
@@ -17,59 +24,59 @@ public class AssetServiceBean implements AssetService {
 
     @Override
     public boolean isCFRInFleetUnderFlagStateOnLandingDate(String cfr, String flagState, DateTime landingDate) {
-//        try {
-//            List<Asset> assetHistories = helper.findHistoryOfAssetByCfr(cfr);
-//
-//            Optional<Asset> historyOnDate = findAssetHistoryByDate(landingDate.toDate(), assetHistories);
-//
-//            if (!historyOnDate.isPresent()) {
-//                // when there's no entry for the specified date, return false
-//                return false;
-//            }
-//
-//            Asset asset = historyOnDate.get();
-//            return asset.getCountryCode().equals(flagState);
-//
-//        } catch (MessageException | AssetModelMarshallException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            List<Asset> assetHistories = helper.findHistoryOfAssetByCfr(cfr);
+
+            Optional<Asset> historyOnDate = findAssetHistoryByDate(landingDate.toDate(), assetHistories);
+
+            if (!historyOnDate.isPresent()) {
+                // when there's no entry for the specified date, return false
+                return false;
+            }
+
+            Asset asset = historyOnDate.get();
+            return asset.getCountryCode().equals(flagState);
+
+        } catch (MessageException | AssetModelMarshallException e) {
+            e.printStackTrace();
+        }
         return true;
     }
-//
-//    protected Optional<Asset> findAssetHistoryByDate(Date landingDate, List<Asset> assetHistories) {
-//        // Asset module sorts this list already, but we do it again here just to be sure
-//        assetHistories.sort(assetComparator());
-//
-//        Asset historyOnDate = null;
-//
-//        // Because our list is ordered from newest event to oldest,
-//        // we can simply check if the date of the event is before the landing date
-//        for (Asset assetHistory : assetHistories) {
-//            if (assetHistory.getEventHistory().getEventDate().before(landingDate)) {
-//                historyOnDate = assetHistory;
-//                break;
-//            }
-//        }
-//
-//        return Optional.fromNullable(historyOnDate);
-//    }
-//
-//
-//    protected Comparator<Asset> assetComparator() {
-//        return new Comparator<Asset>() {
-//            @Override
-//            public int compare(Asset o1, Asset o2) {
-//                if (o1.getEventHistory().getEventDate() == null && o2.getEventHistory().getEventDate() == null) {
-//                    return 0;
-//                } else if (o2.getEventHistory().getEventDate() == null) {
-//                    return 1;
-//                } else if (o1.getEventHistory().getEventDate() == null) {
-//                    return -1;
-//                } else {
-//                    return (o1.getEventHistory().getEventDate().compareTo(o2.getEventHistory().getEventDate()) > 0 ? -1 : 1);
-//                }
-//            }
-//        };
-//    }
+
+    protected Optional<Asset> findAssetHistoryByDate(Date landingDate, List<Asset> assetHistories) {
+        // Asset module sorts this list already, but we do it again here just to be sure
+        assetHistories.sort(assetComparator());
+
+        Asset historyOnDate = null;
+
+        // Because our list is ordered from newest event to oldest,
+        // we can simply check if the date of the event is before the landing date
+        for (Asset assetHistory : assetHistories) {
+            if (assetHistory.getEventHistory().getEventDate().before(landingDate)) {
+                historyOnDate = assetHistory;
+                break;
+            }
+        }
+
+        return Optional.fromNullable(historyOnDate);
+    }
+
+
+    protected Comparator<Asset> assetComparator() {
+        return new Comparator<Asset>() {
+            @Override
+            public int compare(Asset o1, Asset o2) {
+                if (o1.getEventHistory().getEventDate() == null && o2.getEventHistory().getEventDate() == null) {
+                    return 0;
+                } else if (o2.getEventHistory().getEventDate() == null) {
+                    return 1;
+                } else if (o1.getEventHistory().getEventDate() == null) {
+                    return -1;
+                } else {
+                    return (o1.getEventHistory().getEventDate().compareTo(o2.getEventHistory().getEventDate()) > 0 ? -1 : 1);
+                }
+            }
+        };
+    }
 
 }
--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/bean/sales/helper/AssetServiceBeanHelper.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/bean/sales/helper/AssetServiceBeanHelper.java
@@ -1,38 +1,51 @@
 package eu.europa.ec.fisheries.uvms.rules.service.bean.sales.helper;
 
 
+import eu.europa.ec.fisheries.uvms.asset.model.exception.AssetModelMarshallException;
+import eu.europa.ec.fisheries.uvms.asset.model.mapper.AssetModuleRequestMapper;
+import eu.europa.ec.fisheries.uvms.asset.model.mapper.JAXBMarshaller;
+import eu.europa.ec.fisheries.uvms.commons.message.api.MessageException;
+import eu.europa.ec.fisheries.uvms.rules.message.constants.DataSourceQueue;
+import eu.europa.ec.fisheries.uvms.rules.message.consumer.RulesResponseConsumer;
+import eu.europa.ec.fisheries.uvms.rules.message.producer.RulesMessageProducer;
+import eu.europa.ec.fisheries.wsdl.asset.module.FindAssetHistoriesByCfrModuleResponse;
+import eu.europa.ec.fisheries.wsdl.asset.types.Asset;
+
+import javax.ejb.EJB;
 import javax.ejb.Singleton;
+import javax.jms.TextMessage;
+import java.util.List;
 
 @Singleton
 public class AssetServiceBeanHelper {
 
-//    public static final long TIME_TO_WAIT_FOR_A_RESPONSE = 30000L;
-//
-//    @EJB
-//    private RulesMessageProducer messageProducer;
-//
-//    @EJB
-//    private RulesResponseConsumer messageConsumer;
-//
-//    protected List<Asset> receiveMessageFromAsset(String correlationId) throws MessageException, AssetModelMarshallException {
-//        TextMessage receivedMessage = messageConsumer.getMessage(correlationId, TextMessage.class, TIME_TO_WAIT_FOR_A_RESPONSE);
-//        return unmarshal(receivedMessage);
-//    }
-//
-//    protected String sendMessageToAsset(String request) throws MessageException {
-//        return messageProducer.sendDataSourceMessage(request, DataSourceQueue.ASSET, TIME_TO_WAIT_FOR_A_RESPONSE + 1000L);
-//    }
-//
-//    protected List<Asset> unmarshal(TextMessage message) throws AssetModelMarshallException {
-//        FindAssetHistoriesByCfrModuleResponse findAssetHistoriesByCfrModuleResponse = JAXBMarshaller.unmarshallTextMessage(message, FindAssetHistoriesByCfrModuleResponse.class);
-//
-//        return findAssetHistoriesByCfrModuleResponse.getAssetHistories();
-//    }
-//
-//    public List<Asset> findHistoryOfAssetByCfr(String cfr) throws MessageException, AssetModelMarshallException {
-//        String checkForUniqueIdRequest = AssetModuleRequestMapper.createFindAssetByCfrRequest(cfr);
-//        String correlationID = sendMessageToAsset(checkForUniqueIdRequest);
-//
-//        return receiveMessageFromAsset(correlationID);
-//    }
+    public static final long TIME_TO_WAIT_FOR_A_RESPONSE = 30000L;
+
+    @EJB
+    private RulesMessageProducer messageProducer;
+
+    @EJB
+    private RulesResponseConsumer messageConsumer;
+
+    protected List<Asset> receiveMessageFromAsset(String correlationId) throws MessageException, AssetModelMarshallException {
+        TextMessage receivedMessage = messageConsumer.getMessage(correlationId, TextMessage.class, TIME_TO_WAIT_FOR_A_RESPONSE);
+        return unmarshal(receivedMessage);
+    }
+
+    protected String sendMessageToAsset(String request) throws MessageException {
+        return messageProducer.sendDataSourceMessage(request, DataSourceQueue.ASSET, TIME_TO_WAIT_FOR_A_RESPONSE + 1000L);
+    }
+
+    protected List<Asset> unmarshal(TextMessage message) throws AssetModelMarshallException {
+        FindAssetHistoriesByCfrModuleResponse findAssetHistoriesByCfrModuleResponse = JAXBMarshaller.unmarshallTextMessage(message, FindAssetHistoriesByCfrModuleResponse.class);
+
+        return findAssetHistoriesByCfrModuleResponse.getAssetHistories();
+    }
+
+    public List<Asset> findHistoryOfAssetByCfr(String cfr) throws MessageException, AssetModelMarshallException {
+        String checkForUniqueIdRequest = AssetModuleRequestMapper.createFindAssetByCfrRequest(cfr);
+        String correlationID = sendMessageToAsset(checkForUniqueIdRequest);
+
+        return receiveMessageFromAsset(correlationID);
+    }
 }
