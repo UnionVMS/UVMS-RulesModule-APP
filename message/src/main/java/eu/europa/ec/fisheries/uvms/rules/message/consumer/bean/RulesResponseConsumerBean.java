f2f538528632d5674603b5b75424ad244ea7880b
emrinalgr <rinaldi.gregory@gmail.com>
2018-11-26 14:24:36 +0100

    clean

--- message/src/main/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesResponseConsumerBean.java
+++ message/src/main/java/eu/europa/ec/fisheries/uvms/rules/message/consumer/bean/RulesResponseConsumerBean.java
@@ -14,40 +14,16 @@ package eu.europa.ec.fisheries.uvms.rules.message.consumer.bean;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
-import javax.jms.TextMessage;
 import eu.europa.ec.fisheries.uvms.commons.message.api.MessageConstants;
-import eu.europa.ec.fisheries.uvms.commons.message.api.MessageException;
 import eu.europa.ec.fisheries.uvms.commons.message.impl.AbstractConsumer;
-import eu.europa.ec.fisheries.uvms.config.exception.ConfigMessageException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Stateless
 @LocalBean
 public class RulesResponseConsumerBean extends AbstractConsumer {
-
-    private static final Logger LOG = LoggerFactory.getLogger(RulesResponseConsumerBean.class);
-
+    
     @Override
     public String getDestinationName() {
         return MessageConstants.QUEUE_RULES;
-    }
-
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public TextMessage getMessage(final String correlationId, final Long timeoutInMillis) throws MessageException {
-        return getMessage(correlationId, TextMessage.class, timeoutInMillis);
-    }
-
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public <T> T getConfigMessage(String correlationId, Class type) throws ConfigMessageException {
-        try {
-            return getMessage(correlationId, type);
-        } catch (MessageException e) {
-            LOG.error("[ Error when getting config message. ] {}", e.getMessage());
-            throw new ConfigMessageException("[ Error when getting config message. ]");
-        }
     }
 
 }
