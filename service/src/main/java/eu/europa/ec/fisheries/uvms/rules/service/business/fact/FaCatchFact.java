84ab5df64f08fe5142e5bd85e12d78596f2c7076
nomikosi <42890529+nomikosi@users.noreply.github.com>
2020-10-09 22:43:21 +0300

    [UNIONVMS-4798] main area rules

--- service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/fact/FaCatchFact.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/rules/service/business/fact/FaCatchFact.java
@@ -14,7 +14,9 @@
 package eu.europa.ec.fisheries.uvms.rules.service.business.fact;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import eu.europa.ec.fisheries.schema.rules.template.v1.FactType;
 import eu.europa.ec.fisheries.uvms.rules.service.business.AbstractFact;
@@ -24,6 +26,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._20.AAPProcess;
 import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._20.AAPProduct;
+import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._20.FLUXCharacteristic;
 import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._20.FLUXLocation;
 
 @Data
@@ -51,14 +54,77 @@ public class FaCatchFact extends AbstractFact {
     private CodeType categoryCode;
     private List<NumericType> appliedAAPProcessConversionFactorNumber;
     private List<FLUXLocation> specifiedFLUXLocations;
+    private List<FLUXLocation> fishingActivityRelatedFLUXLocations;
+    private List<FLUXLocation> faCatchSpecifiedFLUXLocations;
     private List<CodeType> specifiedFluxLocationRFMOCodeList;
     private List<AAPProcess> appliedAAPProcess;
     private List<FLUXLocation> destinationFLUXLocations;
     private List<IdType> faCatchFluxLocationId;
     private List<IdType> fishActRelatedFluxLocationIds;
+    private List<FLUXCharacteristic>  fishingActivityFluxCharacteristic;
+    private List<FLUXCharacteristic>  faCatchFluxCharacteristic;
     private boolean isSubActivity = false;
 
 
+
+
+    public boolean containsAtMostXTypesOfLocation(List<FLUXLocation> locations,String type,int times){
+
+        if(CollectionUtils.isEmpty(locations)){
+            return true;
+        }
+
+        int i =0;
+
+        for(FLUXLocation location:locations){
+            if(type.equals(location.getTypeCode().getValue())){
+                i ++;
+            }
+
+            if (i > times){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public boolean hasDuplicateLocationType(List<FLUXLocation> locations){
+        Map<String,Integer> map = new HashMap<>();
+
+        if(CollectionUtils.isEmpty(locations)){
+            return false;
+        }
+
+        for(FLUXLocation location:locations){
+
+            if(map.get(location.getTypeCode().getValue()) == null){
+                map.put(location.getTypeCode().getValue(),1);
+            } else {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    public boolean fluxCharacteristicContainsType(List<FLUXCharacteristic> fluxCharacteristics,String value){
+
+        if(CollectionUtils.isEmpty(fluxCharacteristics)){
+            return true;
+        }
+
+        for(FLUXCharacteristic fluxCharacteristic:fluxCharacteristics){
+
+            if(value.equals(fluxCharacteristic.getTypeCode().getValue())){
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public boolean containsAtLeastOneGfcmGsaWithValidValue(List<IdType> ids){
         if(CollectionUtils.isEmpty(ids)){
